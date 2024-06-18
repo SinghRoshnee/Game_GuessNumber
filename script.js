b00c8f1ce5aefd9b062f34633ad6eedb81ff729b
SinghRoshnee <singhroshni012001@gmail.com>
2024-06-18 13:06:32 +0530

    updated

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ btn.addEventListener("click", () => {
   container.style.height = "90vh";
   lowercontent.style.display = "flex"; 
 });
+
+/* main game code */
+
+const randomnumber = parseInt( Math.random() *100 +1)
+console.log(randomnumber);
+
+const submitbtn = document.querySelector('#submitbtn');
+
+submitbtn.addEventListener('click', function(e){
+  e.preventDefault();
+
+  
+})
